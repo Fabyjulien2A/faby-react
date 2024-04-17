6e2e93a03e344044253db948e2333a3ccdf00bf5
Julien Faby <julienfaby@gmail.com>
2024-04-17 13:33:56 +0200

    mise à jour

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -2,8 +2,10 @@ import React from 'react';
 
 export default function HomePage() {
   return (
-    <div>
+    <div className='bg-pink-50'>
       Bienvenue sur ma page d'accueil
     </div>
   );
 }
+
+
